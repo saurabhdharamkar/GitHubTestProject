9d8481bf4557052b21063cf725ee182684a077c1
saurabhd10 <saurabhdharamkar2714@gmail.com>
2021-09-16 12:28:33 -0700

    First Commit

---
+++ GitTestProject/src/com/neosoft/GitHubTest.java
@@ -0,0 +1,11 @@
+package com.neosoft;
+
+public class GitHubTest {
+ public static void main(String[] args) {
+	 String code="SE";
+			 if(code=="SE")
+	System.out.println("SE");
+
+	System.out.println(" Not SE");
+}
+}
